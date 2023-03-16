39d8786541402753d8b616ccd6587914f6024072
alexgithubcoding <127157025+alexgithubcoding@users.noreply.github.com>
2023-03-17 09:55:15 +1300

    Create openLogExample.ino
    added example with Mr Hansen

---
+++ openLogExample/openLogExample.ino
@@ -0,0 +1,34 @@
+
+#include <Wire.h>
+#include "SparkFun_Qwiic_OpenLog_Arduino_Library.h"
+OpenLog myLog; //Create instance
+
+
+void setup() {
+  Wire.begin(); //Initialize I2C
+  myLog.begin(); //Open connection to OpenLog (no pun intended)
+
+  Serial.begin(9600); //9600bps is used for debug statements
+
+  String myFile = "Alex.txt";
+
+  //Get size of file
+  long sizeOfFile = myLog.size(myFile);
+
+  if (sizeOfFile == -1) {
+    myLog.create(myFile);
+    //Record something to the default log
+    myLog.println("This goes to the log file");
+    myLog.syncFile();
+  } else {
+
+  }
+  myLog.append(myFile);
+  myLog.println("This also goes to the log file");
+
+  myLog.syncFile();
+}
+
+void loop() {
+
+}
